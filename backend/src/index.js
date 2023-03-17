a141327fe478d8f52c36bf83bf9227b35f3a7e87
Sungwook Choi <sungwook.csw@gmail.com>
2023-03-17 23:13:58 +0900

    chore: try to connect mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,33 @@ import Koa from 'koa'
 import Router from 'koa-router'
 import api from './api/index.js'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import bodyParser from 'koa-bodyparser'
 
-dotenv.config({path: '.local.env'})
+dotenv.config({path: '.env.local'})
+
+if (process.env.MONGO_URI === undefined) {
+  process.exit(0)
+}
+
+mongoose.connect(process.env.MONGO_URI).then(
+  response => {
+    console.log('Successfully connected to mongodb')
+  }
+).catch(e => {
+  console.error(e)
+})
 
 const app = new Koa()
 const router = new Router()
+const port = process.env.PORT || 4000
+
+app.use(bodyParser())
 
 router.use('/api', api.routes())
-
 app.use(router.routes()).use(router.allowedMethods)
 
 app.listen(4000, () => {
-  console.log('server is listening to port 4000')
+  console.log('server is listening to port ' + port)
+  console.log('hi')
 })
