e0a7caba719e2f9f54a60df1df3984bff9fc004c
Sungwook Choi <sungwook.csw@gmail.com>
2023-03-20 23:38:09 +0900

    feat: add replace logic

--- backend/src/api/books/books.controller.js
+++ backend/src/api/books/books.controller.js
@@ -1,7 +1,35 @@
+import * as Joi from 'joi'
+import mongoose from "mongoose"
 import Book from './../models/book.js'
+
+
+const ObjectId = mongoose.Types.ObjectId
 
 const listBook = (ctx) => {
   ctx.body = 'listed'
+}
+
+const getBook = async (ctx) => {
+  const {id} = ctx.params
+  let book
+
+  try {
+    book = await Book.findById(id).exec()
+  } catch (e) {
+    if (e.name === 'CastError') {
+      ctx.status = 400
+      return
+    }
+    return ctx.throw(500, e)
+  }
+
+  if (!book) {
+    ctx.status = 404
+    ctx.body = {message: 'book not found'}
+    return
+  }
+
+  ctx.body = book
 }
 
 const createBook = async (ctx) => {
@@ -12,28 +40,73 @@ const createBook = async (ctx) => {
   const book = new Book({
     title, authors, publishedDate, price, tags
   })
-<<<<<<< HEAD
-
   try {
     await book.save();
   } catch (e) {
     return ctx.throw(500, e);
   }
-
-=======
->>>>>>> 8902872b6c86febcc15e92d253532fdaf5a5bcb4
 }
 
-const deleteBook = (ctx) => {
-  ctx.body = 'deleted'
+const deleteBook = async (ctx) => {
+  const {id} = ctx.params;
+
+  try {
+    await Book.findByIdAndRemove(id).exec()
+  } catch (e) {
+    if (e.name === 'CastError') {
+      ctx.status = 400
+      return
+    }
+  }
+
+  ctx.status = 204
 }
 
-const replaceBook = (ctx) => {
-  ctx.body = 'replaced'
+const replaceBook = async (ctx) => {
+  const {id} = ctx.params
+
+  if (!ObjectId.isValid((id))) {
+    ctx.status = 400
+    return
+  }
+
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    authors: Joi.array().items(Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email().required()
+    })),
+    publishedDate: Joi.date().required(),
+    price: Joi.number().required(),
+    tags: Joi.array().items((Joi.string()).required())
+  })
+
+
+  const result = Joi.validate(ctx.request.body, schema)
+
+  if (result.error) {
+    ctx.status = 400
+    ctx.body = result.error
+    return
+  }
+
+  let book
+
+  try {
+    book = await Book.findByIdAndUpdate(id, ctx.request.body, {
+      upsert: true,
+      new: true
+    })
+  } catch (e) {
+    return ctx.throw(500, e)
+  }
+
+  ctx.body = book
 }
 
-const updateBook = (ctx) => {
+
+const updateBook = async (ctx) => {
   ctx.body = 'updated'
 }
 
-export {listBook, createBook, deleteBook, replaceBook, updateBook};
+export {listBook, createBook, getBook, deleteBook, replaceBook, updateBook};
--- backend/src/api/books/index.js
+++ backend/src/api/books/index.js
@@ -4,6 +4,8 @@ import * as booksCtrl from './books.controller.js'
 
 const books = new Router()
 
+books.get('/', booksCtrl.listBook)
+books.get('/:id', booksCtrl.getBook)
 books.post('/', booksCtrl.createBook)
 books.delete('/', booksCtrl.deleteBook)
 books.put('/', booksCtrl.replaceBook)
