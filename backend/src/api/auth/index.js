ab6d0b8f2f28e977b04dc7223b8da62fc98bbd22
Sungwook Choi <sungwook.csw@gmail.com>
2023-04-04 15:40:59 +0900

    feat: add some auth routes

--- backend/src/api/auth/index.js
+++ backend/src/api/auth/index.js
@@ -4,5 +4,8 @@ import * as authCtrl from './auth.controller.js'
 const auth = new Router()
 
 auth.post('/register/local', authCtrl.localRegister)
+auth.post('/login/local', authCtrl.localLogin);
+auth.get('/exists/:key(email|username)/:value', authCtrl.exists);
+auth.post('/logout', authCtrl.logout);
 
 export default auth
